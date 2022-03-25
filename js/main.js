b4200d92c3f49a1529cc10852d68b0e23f516df5
Rybus <artem.amerov@gmail.com>
2022-03-25 21:13:06 +0200

    Добавляет функции от Кекса

--- js/main.js
+++ js/main.js
@@ -14,8 +14,19 @@ const intFromRange = function (minRange, maxRange) {
 //stringLimit - функция проверки максимальной длинны строки
 
 const stringLimit = function (verifiedText, maxLength) {
-  if (verifiedText.length <= maxLength ) {
-    return true;
-  }
-  return false;
+  return (verifiedText.length <= maxLength);
 };
+
+
+// Функции от Кекса
+
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
